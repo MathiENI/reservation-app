b28a70bc89dde9d31d9a5b4adb473f934f3f1cf1
Mathilde <mathilde.fauveau2025@campus-eni.fr>
2026-03-20 09:43:47 +0100

    add catalog/reservations models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Resource(models.Model):
+    RESOURCE_TYPE_CHOICES = [
+        ("room", "Salle"),
+        ("equipment", "Équipement"),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=20, choices=RESOURCE_TYPE_CHOICES)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Resource
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+class Reservation(models.Model):
+
+    STATUS_CHOICES = [
+        ("confirmed", "Confirmée"),
+        ("cancelled", "Annulée"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="confirmed")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        #Validation des dates
+        if self.start_datetime >= self.end_datetime:
+            raise ValidationError("La date de fin doit être après la date de début.")
+
+        if self.start_datetime < timezone.now():
+            raise ValidationError("Impossible de réserver dans le passé.")
+
+        #Vérification des conflits
+        conflicts = Reservation.objects.filter(
+            resource=self.resource,
+            start_datetime__lt=self.end_datetime,
+            end_datetime__gt=self.start_datetime,
+            status="confirmed"
+        ).exclude(id=self.id)
+
+        if conflicts.exists():
+            raise ValidationError("Ce créneau est déjà réservé.")
+
+    def __str__(self):
+        return f"{self.resource} - {self.start_datetime}"
